2f4af871accdecb4ae29afd8a36f5e0456798961
Ernest Prabhakar <ernest@quiltdata.io>
2023-06-11 21:46:51 -0700

    pass test_arrow_s3

--- tests/test_arrow.py
+++ tests/test_arrow.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pyarrow.json as pj
+from pyarrow import fs
 
 test_file = 'tests/test.parquet'
 
-def test_arrow():
+def test_arrow_pandas():
     df = pd.DataFrame({'one': [-1, np.nan, 2.5],
                    'two': ['foo', 'bar', 'baz'],
                    'three': [True, False, True]},
@@ -15,3 +17,14 @@ def test_arrow():
     table2 = pq.read_table('example.parquet')
     assert table.equals(table2)
 
+def test_arrow_s3():
+    S3_URI = 's3://quilt-example/.quilt/packages/00004ceff627cc6679fec2c9d55e16614dc055695fc2e4c85f02c0845bfda12f'
+    s3, path = fs.FileSystem.from_uri(S3_URI)
+    assert s3
+    assert path
+    assert 'packages' in str(path)
+    with s3.open_input_stream(path) as f:
+        table = pj.read_json(f)
+        assert table
+        print(table.schema)
+        assert False
