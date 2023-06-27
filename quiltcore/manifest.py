78202b6f2d410f967f33c061e3bb6b00f8f76929
Ernest Prabhakar <ernest@quiltdata.io>
2023-06-27 16:16:38 -0700

    test_man_list

--- quiltcore/manifest.py
+++ quiltcore/manifest.py
@@ -14,18 +14,18 @@ class CoreManifest(CoreResource):
         with path.open(mode="rb") as fi:
             self.table = pj.read_json(fi)
             self.body = self.setup()
+            self.name_col = self.config.get_str("schema/name", "logical_key")
 
     def setup(self) -> pa.Table:
         first = self.table.take([0]).to_pydict()
-        print(f"first: {first}")
         headers = self.get_dict("schema/headers")
-        print(f"headers: {headers}")
         keys = list(headers.keys())
-        print(f"keys: {keys}")
         for key in keys:
-            print(f"key: {key} <- {first}")
             setattr(self, key, first[key][0])
         return self.table.drop_columns(keys).slice(1)
         
-            
+    def list(self) -> list[str]:
+        """List all names. Use strings to avoid excessive data objects"""
+        names = self.body.column(self.name_col)
+        return names.to_pylist()
 
--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -1,14 +1,22 @@
+from pytest import fixture
 from quiltcore import CoreManifest
 from upath import UPath
 
 from .conftest import TEST_TABLE
 
-
-def test_man():
+@fixture
+def man():
     path = UPath(TEST_TABLE)
-    man = CoreManifest(path)
+    return CoreManifest(path)
+
+def test_man(man: CoreManifest):
     assert man
     assert man.table
-    print(man.table)
     assert man.version == "v0"  # type: ignore
     assert man.body.num_rows == 1
+
+
+def test_man_list(man: CoreManifest):
+    results = man.list()
+    assert len(results) == 1
+    assert results == ["camera-reviews"]
