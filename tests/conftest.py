fcfb2767178bf603af61228cdd912ca978b836d4
Ernest Prabhakar <ernest@quiltdata.io>
2023-07-04 21:19:51 -0700

    local tests/example registry

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,26 @@
-TEST_BKT = "s3://quilt-example"
-TEST_PKG = "akarve/amazon-reviews"
-TEST_TAG = "1570503102"
-TEST_HASH = "ffe323137d0a84a9d1d6f200cecd616f434e121b3f53a8891a5c8d70f82244c2"
-TEST_CAT = "https://open.quiltdata.com"
 
-TEST_URL = f"{TEST_CAT}/b/{TEST_BKT}/tree/.quilt/named_packages/{TEST_PKG}/{TEST_TAG}"
-TEST_TABLE = f"s3://quilt-example/.quilt/packages/{TEST_HASH}"
-TEST_KEY = "camera-reviews"
+from pathlib import Path
+TEST_REG = "tests/example"
+TEST_BKT = (Path.cwd() / TEST_REG).as_uri()
+TEST_PKG = "manual/force"
+TEST_TAG = "1680720661"
+TEST_HASH = "3210f808ac0467726439191eea3bd0a66ab261122ee55758367620fedc77fe08"
+TEST_SIZE = 30
+# TEST_CAT = "https://open.quiltdata.com"
+
+# TEST_URL = f"{TEST_CAT}/b/{TEST_BKT}/tree/.quilt/named_packages/{TEST_PKG}/{TEST_TAG}"
+TEST_TABLE = f"{TEST_BKT}/.quilt/packages/{TEST_HASH}"
+TEST_KEY = "ONLYME.md"
 TEST_VER = "ihb.qioVby3gnRaMRFLNsxcX.Yir_K14"
 TEST_OBJ = f"{TEST_BKT}/{TEST_PKG}/{TEST_KEY}?versionId={TEST_VER}"
-TEST_OBJ_HASH = "c833f360052b40b4cb82ba40e4ea702355ad3d92c2bd9a1ba2c2436de9c637ff"
+TEST_OBJ_HASH = "df3e419dfd21f653651a5131e17bf41d82a9fd72baf2a93f634773353bd9d6c8"
+
+TEST_ROW = {
+    "logical_key": [TEST_KEY],
+    "physical_keys": [[TEST_OBJ]],
+    "size": [TEST_SIZE],
+    "hash": {
+        "value": [TEST_OBJ_HASH],
+        "type": ["SHA256"],
+    }
+}
