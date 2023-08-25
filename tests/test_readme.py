92221915a0b4d2e002c1353429114fd4d83fc03e
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-08-25 09:10:20 -0700

    Improve testing (#45)
    * new role
    
    * workflow examples
    
    * Update poetry.lock
    
    * test_readme for better errors

---
+++ tests/test_readme.py
@@ -0,0 +1,24 @@
+from quiltcore import Registry
+from tempfile import TemporaryDirectory
+from upath import UPath
+
+TEST_BKT = "s3://quilt-example"
+TEST_PKG = "akarve/amazon-reviews"
+TEST_TAG = "1570503102"
+TEST_HASH = "ffe323137d0a84a9d1d6f200cecd616f434e121b3f53a8891a5c8d70f82244c2"
+TEST_KEY = "camera-reviews"
+
+def test_readme():
+    path = UPath(TEST_BKT)
+    registry = Registry(path)
+    named_package = registry.get(TEST_PKG)
+    manifest = named_package.get(TEST_TAG)
+    entry = manifest.get(TEST_KEY)
+    with TemporaryDirectory() as tmpdir:
+        dest = UPath(tmpdir)
+        outfile = dest / TEST_KEY
+        entry.get(tmpdir)
+        print(outfile.resolve())
+        assert outfile.exists()
+        local_bytes = outfile.read_bytes()
+        assert entry.verify(local_bytes)
