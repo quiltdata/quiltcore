3c06054c8af49eb4397c7e8b2a6ca00e5fb61763
Ernest Prabhakar <ernest@quiltdata.io>
2023-10-06 18:46:22 -0700

    domain._status

--- quiltcore/domain.py
+++ quiltcore/domain.py
@@ -39,6 +39,11 @@ class Domain(Folder):
             return parent.store
         return cls.FindStore(parent)
 
+    @staticmethod
+    def TimeStamp() -> str:
+        """Return a timestamp."""
+        return datetime.datetime.now().isoformat()
+
     def __init__(self, name, parent, **kwargs):
         super().__init__(name, parent, **kwargs)
         self.store = self.ToPath(self.parent_name(), name)
@@ -49,22 +54,41 @@ class Domain(Folder):
         self.is_mutable = kwargs.get(self.K_MUTABLE, False)
         self.data_yaml = Data(self.store)
 
-    def pull(self, udi: UDI, dest: UPath|None=None, **kwargs):
+    def pull(self, udi: UDI, dest: UPath | None = None, **kwargs):
         """Pull resource at the UDI into the domain."""
         assert self.is_mutable, "Can not pull into read-only Domain"
         path = dest or UPath(udi.package)
         self._track_lineage("pull", udi, path, **kwargs)
         return self.cf.AsStr(path)
 
+    def _status(self, attrs: dict, **kwargs) -> dict:
+        """Return the status dictionary for this UDI event."""
+        status = {
+            "timestamp": self.TimeStamp(),
+            "user": os.getlogin(),
+            **attrs,
+            **kwargs,
+        }
+        return status
+
     def _track_lineage(self, action, udi: UDI, path: UPath, **kwargs):
         """Store the UDI in the domain."""
         dest = self.cf.AsStr(path)
         uri = udi.uri
         assert uri and isinstance(uri, str)
-        kwargs["timestamp"] = datetime.datetime.now().isoformat()
-        kwargs["user"] = os.environ.get("USER", "unknown")
-        logging.debug(f"Domain._track_lineage: {action} {udi} {kwargs}")
-        self.data_yaml.set(dest, uri, action, kwargs)
+        # TODO: store hash and other udi attributes
+        opts = self._status(udi.attrs, **kwargs)
+        logging.debug(f"Domain._track_lineage: {action} {udi} {opts}")
+        self.data_yaml.set(dest, uri, action, opts)
         self.data_yaml.save()
         return True
-    
+
+    def _remote_manifest(self, udi: UDI) -> Manifest2:
+        """Return the manifest for the remote."""
+        remote = Domain.FromURI(udi.uri)
+        manifest = remote.get(udi.package)
+        return manifest
+
+    def _translate_manifest(self, manifest: Manifest2) -> Manifest2:
+        """Translate the manifest to the local domain."""
+        return manifest
