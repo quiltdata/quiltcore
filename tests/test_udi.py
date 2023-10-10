741239f4a8922ff683b918160a4290821a4e54fc
Ernest Prabhakar <ernest@quiltdata.io>
2023-10-10 11:27:33 -0700

    remove ExpandRelative

--- tests/test_udi.py
+++ tests/test_udi.py
@@ -1,6 +1,5 @@
 import pytest
 
-from pathlib import Path
 from quiltcore import UDI
 
 from .conftest import BKT_URI, PTH_URI, T_BKT, T_PKG, TEST_URI, LOCAL_UDI
@@ -41,13 +40,3 @@ def test_udi_localhost():
     assert local
     assert local.uri == LOCAL_UDI
     assert "localhost" not in local.registry
-
-
-def test_udi_relative():
-    UDI_DOT = "quilt+file://./test"
-    udi = UDI.FromUri(UDI_DOT)
-    assert udi.registry == f"file://{Path.cwd()}/test"
-
-    UDI_SUB = "quilt+file://./test/example"
-    udi = UDI.FromUri(UDI_SUB)
-    assert udi.registry == f"file://{Path.cwd()}/test/example"
