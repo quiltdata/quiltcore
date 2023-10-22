fa1c1bd88ed7a52087097c2eba3fcec500c625f0
Ernest Prabhakar <ernest@quiltdata.io>
2023-10-22 23:04:36 +0200

    pass readme

--- tests/readme.py
+++ tests/readme.py
@@ -1,26 +1,3 @@
-# QuiltCore
-
-QuiltCore is a library for building and running [Quilt](https://quiltdata.com) data packages.
-It is designed to leverage standard open source technology and YAML configuration files
-so that it can easily be ported to other languages and platforms.
-
-This initial implementation is in Python.
-
-## Key Technologies
-
-- Apache [Arrow](https://arrow.apache.org/) for reading, writing, and representing manifests
-  - [PyArrow](https://arrow.apache.org/docs/python/) for Python bindings to Arrow
-- fsspec [filesystems](https://filesystem-spec.readthedocs.io/en/latest/)
-  for reading and writing files from various sources
-- [PyYAML](https://pyyaml.org/) for reading and writing YAML configuration files
-
-## Example
-
-```bash
-poetry install
-```
-
-```python
 #!/usr/bin/env python
 import os
 from quiltcore import Domain, UDI
@@ -35,12 +12,7 @@ TEST_KEY = "camera-reviews"
 WRITE_BKT = os.environ.get("WRITE_BUCKET")
 SOURCE_URI = f"quilt+{TEST_BKT}#package={TEST_PKG}:{TEST_TAG}"
 DEST_URI = f"quilt+{TEST_BKT}#package={TEST_PKG}:{TEST_TAG}"
-```
 
-### Get Manifest
-
-<!--pytest-codeblocks:cont-->
-```python
 remote = UDI.FromUri(SOURCE_URI)
 print(f"remote: {remote}")
 with TemporaryDirectory() as tmpdir:
@@ -52,4 +24,3 @@ with TemporaryDirectory() as tmpdir:
     if WRITE_BKT:
         tag = domain.push(folder, remote=UDI.FromUri(DEST_URI))
         print(f"tag: {tag}")
-```
